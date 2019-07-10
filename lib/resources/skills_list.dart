d2b22714cb45b177736992d93821d4596702e66e
Ryan Lertola <rlertola@gmail.com>
2019-07-09 23:29:34 -0500

    Fixed landscape view

--- lib/resources/skills_list.dart
+++ lib/resources/skills_list.dart
@@ -14,22 +14,23 @@ List skills = [
   {'id': 8, 'title': 'REST API', 'logo': 'assets/skills_icons/restapi80.png'},
   {'id': 9, 'title': 'Dart', 'logo': 'assets/skills_icons/dart80.png'},
   {'id': 10, 'title': 'Flutter', 'logo': 'assets/skills_icons/flutter80.png'},
-  {'id': 11, 'title': 'MongoDB', 'logo': 'assets/skills_icons/mongodb80.png'},
-  {'id': 12, 'title': 'Mongoose', 'logo': 'assets/skills_icons/mongoose76.png'},
-  {'id': 13, 'title': 'Firebase', 'logo': 'assets/skills_icons/firebase80.png'},
-  {'id': 14, 'title': 'MySQL', 'logo': 'assets/skills_icons/mysql80.png'},
-  {'id': 15, 'title': 'GitHub', 'logo': 'assets/skills_icons/github80.png'},
-  {'id': 16, 'title': 'VS Code ', 'logo': 'assets/skills_icons/vscode80.png'},
+  {'id': 11, 'title': 'Kotlin', 'logo': 'assets/skills_icons/kotlin76.png'},
+  {'id': 12, 'title': 'MongoDB', 'logo': 'assets/skills_icons/mongodb80.png'},
+  {'id': 13, 'title': 'Mongoose', 'logo': 'assets/skills_icons/mongoose76.png'},
+  {'id': 14, 'title': 'Firebase', 'logo': 'assets/skills_icons/firebase80.png'},
+  {'id': 15, 'title': 'MySQL', 'logo': 'assets/skills_icons/mysql80.png'},
+  {'id': 16, 'title': 'GitHub', 'logo': 'assets/skills_icons/github80.png'},
+  {'id': 17, 'title': 'VS Code ', 'logo': 'assets/skills_icons/vscode80.png'},
   {
-    'id': 17,
+    'id': 18,
     'title': 'Android Studio',
     'logo': 'assets/skills_icons/androidstudio80.png'
   },
   {
-    'id': 18,
+    'id': 19,
     'title': 'Command Line',
     'logo': 'assets/skills_icons/commandline80.png'
   },
-  {'id': 19, 'title': 'NPM', 'logo': 'assets/skills_icons/npm80.png'},
-  {'id': 20, 'title': 'JSON', 'logo': 'assets/skills_icons/json80.png'},
+  {'id': 20, 'title': 'NPM', 'logo': 'assets/skills_icons/npm80.png'},
+  {'id': 21, 'title': 'JSON', 'logo': 'assets/skills_icons/json80.png'},
 ];
--- lib/screens/contact.dart
+++ lib/screens/contact.dart
@@ -19,55 +19,59 @@ class Contact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/logo114.png'),
-            // CircleAvatar(
-            //   backgroundImage: AssetImage(kAvatar),
-            //   radius: 50,
-            //   backgroundColor: Colors.red,
-            // ),
-            PaddingText(
-              padding: EdgeInsets.all(16.0),
-              text: 'Ryan Lertola',
-              size: 45.0,
-              family: 'Raleway',
-              color: Color(0xFFb2ebf9),
-              weight: FontWeight.w100,
-            ),
-            PaddingText(
-              padding: EdgeInsets.only(bottom: 4.0),
-              text: kMainDescription,
-              size: 18.0,
-              family: 'Jura',
-              color: Color(0xFFb2ebf9),
-            ),
-            PaddingText(
-              padding: EdgeInsets.only(bottom: 16.0),
-              text: kSecondaryDescription,
-              size: 18.0,
-              family: 'Jura',
-              color: Color(0xFFb2ebf9),
-            ),
-            SizedBox(
-              height: 20.0,
-              width: 150.0,
-              child: Divider(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(top: 80.0),
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/logo114.png'),
+              // CircleAvatar(
+              //   backgroundImage: AssetImage(kAvatar),
+              //   radius: 50,
+              //   backgroundColor: Colors.red,
+              // ),
+              PaddingText(
+                padding: EdgeInsets.all(16.0),
+                text: 'Ryan Lertola',
+                size: 45.0,
+                family: 'Raleway',
+                color: Color(0xFFb2ebf9),
+                weight: FontWeight.w100,
+              ),
+              PaddingText(
+                padding: EdgeInsets.only(bottom: 4.0),
+                text: kMainDescription,
+                size: 18.0,
+                family: 'Jura',
                 color: Color(0xFFb2ebf9),
               ),
-            ),
-            ContactCard(
-              onTap: () => launchURL(kSMS),
-              titleText: kPhoneNumberText,
-              icon: Icons.chat,
-            ),
-            ContactCard(
-              onTap: () => launchURL(kEmailAddressURL),
-              titleText: kEmailAddress,
-              icon: Icons.email,
-            ),
-          ],
+              PaddingText(
+                padding: EdgeInsets.only(bottom: 16.0),
+                text: kSecondaryDescription,
+                size: 18.0,
+                family: 'Jura',
+                color: Color(0xFFb2ebf9),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Color(0xFFb2ebf9),
+                ),
+              ),
+              ContactCard(
+                onTap: () => launchURL(kSMS),
+                titleText: kPhoneNumberText,
+                icon: Icons.chat,
+              ),
+              ContactCard(
+                onTap: () => launchURL(kEmailAddressURL),
+                titleText: kEmailAddress,
+                icon: Icons.email,
+              ),
+            ],
+          ),
         ),
       ),
     );
